55ae7a4cbe6c2a31a9982108064f0ae92e3bd039
ululfadli99 <76417510+ululfadli99@users.noreply.github.com>
2022-11-29 09:37:55 +0700

    init

---
+++ config/config.candy.php
@@ -0,0 +1,9 @@
+<?php
+$copyright = 'CBT SMK BAKTI PO';
+
+define("VERSI", "2.8.0");
+define("REVISI", "3");
+define("APLIKASI", "CBT SMK BAKTI PO");
+define("NAMA_DATABASE", "ecandy28r3");
+define("VERSI_DB", "2.8.1");
+define("BASEPATH", "cbtsmkbaktipo");
